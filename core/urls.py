0558470c8e5808cb19eb94fd649d6165143db5db
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-07-23 09:45:01 +0500

    Healthy url

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('health/', lambda _: JsonResponse({'detail': 'Healthy'}), name='health'),
 ]
