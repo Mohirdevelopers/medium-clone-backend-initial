ed6484ab1f0319a3abf8de34b3d88257cf84227c
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-07-24 09:03:55 +0500

    static root updated

--- tests/static-fayllarni-sozlash/test_settings.py
+++ tests/static-fayllarni-sozlash/test_settings.py
@@ -4,4 +4,4 @@ from django.conf import settings
 
 def test_constants_exists():
     assert settings.STATIC_URL == '/static/', "STATIC_URL is not set correctly"
-    assert settings.STATIC_ROOT == settings.BASE_DIR / "staticfiles", "STATIC_ROOT is not set correctly"
+    assert settings.STATIC_ROOT == settings.BASE_DIR / "static", "STATIC_ROOT is not set correctly"
