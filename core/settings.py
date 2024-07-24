f3e1ff851f9973327afb78b7e4e5171fff6c8b7a
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-07-24 09:07:06 +0500

    static root updated

--- core/settings.py
+++ core/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
