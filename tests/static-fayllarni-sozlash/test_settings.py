cb33da3d39054de5e68ff085dcce2df98a8ca85e
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-07-24 12:17:17 +0500

    static url test

--- tests/static-fayllarni-sozlash/test_settings.py
+++ tests/static-fayllarni-sozlash/test_settings.py
@@ -1,6 +1,24 @@
+import pytest
 from django.conf import settings
+from django.conf.urls.static import static
+from core.urls import urlpatterns
 
 
 def test_constants_exists():
     assert settings.STATIC_URL == '/static/', "STATIC_URL is not set correctly"
     assert settings.STATIC_ROOT == settings.BASE_DIR / "static", "STATIC_ROOT is not set correctly"
+
+
+static_url = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns = urlpatterns
+
+
+@pytest.mark.django_db
+def test_static_urlpatterns():
+    global static_url
+    global urlpatterns
+
+    pattern_str = static_url[0]
+
+    assert str(pattern_str) == str(urlpatterns[-1])
