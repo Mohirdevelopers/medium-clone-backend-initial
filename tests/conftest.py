72a31995cb7f2d00987093b4d1838f2a3e8654d2
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-07-29 17:05:50 +0500

    articles

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 import fakeredis
-from pytest_factoryboy import register
 
 try:
     from rest_framework.test import APIClient
@@ -11,29 +10,8 @@ try:
     from rest_framework_simplejwt.tokens import RefreshToken
 except ImportError:
     pass
-try:
-    from tests.factories.article_factory import (
-        ArticleFactory, TopicFactory, CommentFactory,
-        FavoriteFactory, ClapFactory, ReadingHistoryFactory,
-        FollowFactory, PinFactory, NotificationFactory, FAQFactory)
-except Exception:
-    pass
-from tests.factories.user_factory import UserFactory
 
-try:
-    register(UserFactory)
-    register(ArticleFactory)
-    register(TopicFactory)
-    register(CommentFactory)
-    register(FavoriteFactory)
-    register(ClapFactory)
-    register(ReadingHistoryFactory)
-    register(FollowFactory)
-    register(PinFactory)
-    register(NotificationFactory)
-    register(FAQFactory)
-except Exception:
-    pass
+
 
 
 @pytest.fixture
