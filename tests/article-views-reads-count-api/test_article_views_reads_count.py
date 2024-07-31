97d5556ba127a64e7ac9a82e9b066804af131b0e
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-07-31 14:48:52 +0500

    views and reads

--- tests/article-views-reads-count-api/test_article_views_reads_count.py
+++ tests/article-views-reads-count-api/test_article_views_reads_count.py
@@ -22,34 +22,6 @@ def article_data(request, user_factory):
 
 @pytest.mark.django_db
 @pytest.mark.order(2)
-def test_view_article(article_data, api_client, tokens):
-    """
-    Test retrieving an article and incrementing the views count.
-    """
-
-    from articles.models import Article, ReadingHistory
-
-    article_id, user = article_data
-
-    access, _ = tokens(user)
-    client = api_client(token=access)
-
-    article = Article.objects.get(id=article_id)
-    initial_views_count = article.views_count
-
-    response = client.get(f'/articles/{article_id}/')
-
-    assert response.status_code == status.HTTP_200_OK
-    assert response.data['id'] == article_id
-
-    article.refresh_from_db()
-    assert article.views_count == initial_views_count + 1
-
-    assert ReadingHistory.objects.filter(user=user, article=article).exists()
-
-
-@pytest.mark.django_db
-@pytest.mark.order(3)
 def test_increment_reads_count(article_data, api_client, tokens):
     """
     Test incrementing the reads count of an article.
@@ -80,7 +52,7 @@ def test_increment_reads_count(article_data, api_client, tokens):
 
 
 @pytest.mark.django_db
-@pytest.mark.order(4)
+@pytest.mark.order(3)
 def test_increment_reads_count_article_not_found(article_data, api_client, tokens):
     """
     Test incrementing reads count for a non-existent article.
@@ -94,3 +66,31 @@ def test_increment_reads_count_article_not_found(article_data, api_client, token
     response = client.post(f'/articles/{non_existent_article_id}/read/')
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+@pytest.mark.order(4)
+def test_view_article(article_data, api_client, tokens):
+    """
+    Test retrieving an article and incrementing the views count.
+    """
+
+    from articles.models import Article, ReadingHistory
+
+    article_id, user = article_data
+
+    access, _ = tokens(user)
+    client = api_client(token=access)
+
+    article = Article.objects.get(id=article_id)
+    initial_views_count = article.views_count
+
+    response = client.get(f'/articles/{article_id}/')
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['id'] == article_id
+
+    article.refresh_from_db()
+    assert article.views_count == initial_views_count + 1
+
+    assert ReadingHistory.objects.filter(user=user, article=article).exists()
