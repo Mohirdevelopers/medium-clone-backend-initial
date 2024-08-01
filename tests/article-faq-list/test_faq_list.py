d1fc47a11f49cc5b895450745bdfed482ca70410
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-08-01 12:23:53 +0500

    faq

---
+++ tests/article-faq-list/test_faq_list.py
@@ -0,0 +1,31 @@
+import pytest
+from rest_framework import status
+
+
+@pytest.mark.order(1)
+def test_faq_model_exists():
+    """
+    Test that the FAQ model exists.
+    """
+    from articles.models import FAQ
+    assert FAQ, "FAQ model not created"
+
+
+@pytest.mark.django_db
+@pytest.mark.order(2)
+def test_faq_list_view(api_client):
+    """
+    Test the FAQListView for retrieving a list of FAQs.
+    """
+
+    from tests.factories.faq_factory import FAQFactory
+
+    FAQFactory.create()
+    FAQFactory.create()
+
+    client = api_client()
+    response = client.get('/articles/faqs/')
+
+    assert response.status_code == status.HTTP_200_OK
+
+    assert len(response.data['results']) == 2
