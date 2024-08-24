809469ff432b83a7b6b4dce9fe095254189cfd79
Abduvohid26 <abdujalilovabduvohid6@gmail.com>
2024-08-24 20:47:31 +0500

    1 lesson finish

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
+from email.policy import default
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -7,10 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bos)q+o=7(ii#0x_-gjnz16(4$jg81lo1sm+ltk@z%0u20*t%g"
+SECRET_KEY = config('SECRET_KEY', defuault='%^#$#dkjfjdjfsdcdc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
