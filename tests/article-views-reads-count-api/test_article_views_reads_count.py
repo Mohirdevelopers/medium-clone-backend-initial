13caf13c963f2f032449c5a72526556d352adb26
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-10-13 15:26:35 +0500

    reads count fixed

--- tests/article-views-reads-count-api/test_article_views_reads_count.py
+++ tests/article-views-reads-count-api/test_article_views_reads_count.py
@@ -39,14 +39,14 @@ def test_increment_reads_count(article_data, api_client, tokens):
 
     response = client.post(f'/articles/{article_id}/read/')
 
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_201_CREATED
     assert response.data['detail'] == "Maqolani o'qish soni ortdi."
 
     article.refresh_from_db()
     assert article.reads_count == initial_reads_count + 1
 
     response = client.post(f'/articles/{article_id}/read/')
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_201_CREATED
     article.refresh_from_db()
     assert article.reads_count == initial_reads_count + 2
 
